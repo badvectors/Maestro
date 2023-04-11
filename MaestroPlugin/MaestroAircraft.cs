ae06b8db00d2acbc879c028c8b85458621d56055
ajdunsmuir <46506175+ajdunsmuir@users.noreply.github.com>
2023-04-12 08:18:54 +1000

    Add project files.

---
+++ MaestroPlugin/MaestroAircraft.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using vatsys;
+using static vatsys.FDP2.FDR;
+
+namespace MaestroPlugin
+{
+    public class MaestroAircraft
+    {
+        public MaestroAircraft() 
+        {
+            LastSeen = DateTime.UtcNow;
+        }
+
+        public MaestroAircraft(FDP2.FDR fdr) : this()
+        {
+            Callsign = fdr.Callsign;
+            Type = fdr.AircraftTypeAndWake?.Type;
+            Wake = fdr.AircraftTypeAndWake?.WakeCategory;
+            FlightRules = fdr.FlightRules;
+            Airport = fdr.DesAirport;
+            Runway = fdr.ArrivalRunway?.Runway?.Name ?? null;
+            STAR = fdr.STAR?.Name ?? null;
+            if (fdr.Trajectory.Any()) ETO1 = fdr.Trajectory.Last()?.ETO ?? null;
+            if (fdr.ParsedRoute != null) ParsedRoute = fdr.ParsedRoute;
+        }
+
+        public void FDRUpdate(FDP2.FDR fdr)
+        {
+            LastSeen = DateTime.UtcNow;
+
+            FlightRules = fdr.FlightRules;
+            Airport = fdr.DesAirport;
+            Runway = fdr.ArrivalRunway?.Runway?.Name ?? null;
+            STAR = fdr.STAR?.Name ?? null;
+            if (fdr.Trajectory.Any()) ETO1 = fdr.Trajectory.Last()?.ETO ?? null;
+            if (fdr.ParsedRoute != null) ParsedRoute = fdr.ParsedRoute;
+        }
+
+        public void RadarUpdate(RDP.RadarTrack radarTrack)
+        {
+            LastSeen = DateTime.UtcNow;
+
+            GroundSpeed = radarTrack.GroundSpeed;
+
+            if (ParsedRoute == null) return;
+
+            double distanceToGo = 0;
+            var lastPos = radarTrack.ActualAircraft.Position;
+
+            foreach (var wpt in ParsedRoute.Where(x => x.ETO > DateTime.UtcNow))
+            {
+                distanceToGo += Conversions.CalculateDistance(lastPos, wpt.Intersection.LatLong);
+                lastPos = wpt.Intersection.LatLong;
+            }
+
+            DistanceToGo = distanceToGo;
+
+            if (GroundSpeed == 0) return;
+
+            HoursToGo = DistanceToGo / GroundSpeed;
+
+            ETO2 = DateTime.UtcNow.AddHours(HoursToGo.Value);
+        }
+
+        public string Callsign { get; set; }
+        public string Type { get; set; }
+        public string FlightRules { get; set; }
+        public string Wake { get; set; }
+        public string Airport { get; set; }
+        public string Runway { get; set; }
+        public string STAR { get; set; }
+        public DateTime? ETO1 { get; set; }
+        public DateTime? ETO2 { get; set; }
+        public double? HoursToGo { get; set; }
+        public double? GroundSpeed { get; set; }
+        public double? DistanceToGo { get; set; }
+        public DateTime LastSeen { get; set; }
+        [JsonIgnore] public ExtractedRoute ParsedRoute { get; set; }
+
+    }
+}
---
+++ MaestroPlugin/MaestroPlugin.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using vatsys;
+using vatsys.Plugin;
+
+namespace MaestroPlugin
+{
+    [Export(typeof(IPlugin))]
+    public class MaestroPlugin : IPlugin
+    {
+        public string Name => nameof(MaestroPlugin);
+        private static BindingList<MaestroAircraft> Aircraft { get; set; } = new BindingList<MaestroAircraft>();
+        private static System.Timers.Timer Timer { get; set; } = new System.Timers.Timer();
+        private static HttpClient Client { get; set; } = new HttpClient();
+
+        private static string Url { get; set; } = "https://localhost:7258/Updates";
+
+        public MaestroPlugin()
+        {
+            Timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            Timer.Interval = 3000;
+            Timer.Enabled = false;
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Remove();
+        }
+
+        public async void OnFDRUpdate(FDP2.FDR updated)
+        {
+            await FDRUpdate(updated);
+        }
+
+        public async void OnRadarTrackUpdate(RDP.RadarTrack updated)
+        {
+            if (updated.CoupledFDR != null) await FDRUpdate(updated.CoupledFDR);
+
+            var existing = Aircraft.FirstOrDefault(x => x.Callsign == updated.ActualAircraft.Callsign);
+
+            if (existing == null) return;
+
+            existing.RadarUpdate(updated);
+
+            await Send(existing);
+        }
+
+        private async Task FDRUpdate(FDP2.FDR updated)
+        {
+            var existing = Aircraft.FirstOrDefault(x => x.Callsign == updated.Callsign);
+
+            if (existing != null)
+            {
+                existing.FDRUpdate(updated);
+
+                await Send(existing);
+            }
+            else
+            {
+                var aircraft = new MaestroAircraft(updated);
+
+                Aircraft.Add(aircraft);
+
+                await Send(aircraft);
+            }
+        }
+
+        private static async Task Send(MaestroAircraft maestroAircraft)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(maestroAircraft);
+
+                var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+                await Client.PostAsync(Url, httpContent);
+            }
+            catch {  }
+        }
+
+        private static void Remove()
+        {
+            foreach (var aircraft in Aircraft)
+            {
+                var target = Aircraft.FirstOrDefault(x => x.Callsign == aircraft.Callsign);
+
+                if (target == null) continue;
+
+                if (FDP2.GetFDRIndex(aircraft.Callsign) == -1)
+                {
+                    Aircraft.Remove(target);
+                    continue;
+                }
+
+                if (aircraft.GroundSpeed <= 30)
+                {
+                    Aircraft.Remove(target);
+                    continue;
+                }
+
+                if (DateTime.UtcNow.Subtract(aircraft.LastSeen) > TimeSpan.FromMinutes(1))
+                {
+                    Aircraft.Remove(target);
+                    continue;
+                }
+            }
+        }
+    }
+}
